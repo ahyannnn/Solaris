bb5edd243cfb2ed4d2196aa8ff6ee35cee4b447d
CarlosONin <padillacarlosnino.pdm@gmail.com>
2026-03-07 19:45:46 +0800

    Describe your changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
+import { useEffect } from "react";
+import { api } from "./services/api";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SolarisLandingPage from './pages/Auth/landingpage';
 import LoginPage from './pages/Auth/loginpage';
 import RegisterPage from './pages/Auth/registerpage';
 import ForgotPage from './pages/Auth/forgotpage';
 import Dashboard from "./pages/Dashboard_Layout/dashboard";
-import AdminDashboard from './pages/Admin/dashboard';
 
 function App() {
   return (
@@ -14,13 +15,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgotpassword" element={<ForgotPage />} />
-        
-        {/* IISA LANG NA ROUTE - /dashboard LANG */}
-        <Route path="/dashboard" element={<Dashboard />} />
-
-        <Route path="/dashboard/AdminDashboard" element={<AdminDashboard />} />
-
-
+        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );

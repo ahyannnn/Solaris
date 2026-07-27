f4a8c6086f845ab9e27f20bedf5e704333448afd
ahyannnn <iangeorgesanico@gmail.com>
2026-07-27 14:48:02 +0800

    add booking reference in sensor data post method

--- server/controllers/sensorController.js
+++ server/controllers/sensorController.js
@@ -2,14 +2,19 @@ const SensorData = require("../models/SensorData");
 
 exports.receiveData = async (req, res) => {
   try {
-    const { deviceId, irradiance, temperature, humidity, gps, timestamp } = req.body;
+    const { deviceId, bookingReference, irradiance, temperature, humidity, gps, timestamp } = req.body;
 
     if (!deviceId) {
       return res.status(400).json({ message: "Device ID is required" });
     }
 
+    if (!bookingReference) {
+      return res.status(400).json({ message: "Booking reference is required" });
+    }
+
     const data = new SensorData({
       deviceId,
+      bookingReference,
       irradiance,
       temperature,
       humidity,
